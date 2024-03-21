76d922d385553a2dc837c3bf36774358b99228ea
Abdul Hadi Khan Khokhar <hadikk0000@gmail.com>
2024-03-21 11:46:18 +0500

    Initial commit

---
+++ bmi.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const inquirer_1 = __importDefault(require("inquirer"));
+const input1 = await inquirer_1.default.prompt({
+    name: "weight",
+    type: "number",
+    message: "What is your wait in Kilogram?\n"
+});
+const input2 = await inquirer_1.default.prompt({
+    name: "height",
+    type: "number",
+    message: "What is your height in meter?\n"
+});
+console.log("About You:");
+let BMI = input1.weight / (input2.height * input2.height);
+if (BMI < 17) {
+    console.log(`Your BMI is: ${BMI}, Eat Healthy Food! You are weak.`);
+}
+else if (BMI >= 17 && BMI < 23) {
+    console.log(`Your BMI is: ${BMI}, You are a Healthy person.`);
+}
+else if (BMI >= 23) {
+    console.log(`Your BMI is: ${BMI}, Avoid Eating Junk Food! You are overweight`);
+}
+else {
+    console.log("Kindly enter correct weight(in kg) and height(in m)");
+}
